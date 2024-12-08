6b2df1b4731a6aabbaf12fad273a7f1954e28e3d
alexp <alexpoirier05@gmail.com>
2024-12-08 12:58:28 -0500

    Réglé les problèmes d'alignement avec la fenêtre de configuration du serveur

--- TP2/BankClient2/src/main/java/com/atoudeft/vue/PanneauConfigServeur.java
+++ TP2/BankClient2/src/main/java/com/atoudeft/vue/PanneauConfigServeur.java
@@ -13,24 +13,33 @@ public class PanneauConfigServeur extends JPanel {
     private JTextField txtAdrServeur, txtNumPort;
 
     public PanneauConfigServeur(String adr, int port) {
-        txtAdrServeur = new JTextField(30);
+        txtAdrServeur = new JTextField(20);
         txtAdrServeur.setText(adr);
-        txtNumPort = new JTextField(30);
+        txtNumPort = new JTextField(20);
         txtNumPort.setText(Integer.toString(port));
         JLabel labelAdrServeur = new JLabel("Adresse IP : ");
+        labelAdrServeur.setHorizontalAlignment(SwingConstants.RIGHT);
         JLabel labelNumPort = new JLabel("Port : ");
+        labelNumPort.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        JPanel pNORTH = new JPanel();
-        JPanel pSOUTH = new JPanel();
-        JPanel pTout = new JPanel(new GridLayout(2,2));
+        JPanel pTextFields = new JPanel();
+        GridLayout textFieldsLayout = new GridLayout(2,1);
+        textFieldsLayout.setVgap(5);
+        pTextFields.setLayout(textFieldsLayout);
+        pTextFields.add(this.txtAdrServeur);
+        pTextFields.add(this.txtNumPort);
 
-        pNORTH.add(labelAdrServeur, BorderLayout.WEST);
-        pNORTH.add(txtAdrServeur, BorderLayout.EAST);
-        pSOUTH.add(labelNumPort, BorderLayout.WEST);
-        pSOUTH.add(txtNumPort, BorderLayout.EAST);
-        pTout.add(pNORTH, BorderLayout.NORTH);
-        pTout.add(pSOUTH, BorderLayout.SOUTH);
-        this.add(pTout);
+        JPanel pLabels = new JPanel();
+        GridLayout labelsLayout = new GridLayout(2,1);
+        labelsLayout.setVgap(5);
+        pLabels.setLayout(labelsLayout);
+        pLabels.add(labelAdrServeur);
+        pLabels.add(labelNumPort);
+
+        JPanel pParent = new JPanel(new BorderLayout());
+        pParent.add(pTextFields, BorderLayout.CENTER);
+        pParent.add(pLabels, BorderLayout.WEST);
+        this.add(pParent);
     }
     public String getAdresseServeur() {
         return txtAdrServeur.getText();
